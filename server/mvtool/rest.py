a58d9e15a463a1dadea8c22b5d19538ae47eba82
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-05-08 12:59:52 +0200

    add handler methods for REST operations

--- server/mvtool/rest.py
+++ server/mvtool/rest.py
@@ -16,12 +16,31 @@
 import tornado.web
 
 
-class EndpointHandler(tornado.web.RequestHandler):
+class RestHandler(tornado.web.RequestHandler):
+    def initialize(self, object_schema):
+        self._object_schema = object_schema
+        self._objects = dict()
+
+    def create_object(self):
+        pass
+
+    def get_object(self, id_):
+        pass
+
+    def update_object(self, id_):
+        pass
+
+    def delete_object(self, id_):
+        pass
+
+    def get_objects(self):
+        pass
+
+    def post(self):
+        pass
+
     def get(self, id_):
         self.write(f"Hello, {id_}")
-
-    def post(self, id_):
-        pass
 
     def put(self, id_):
         pass
