bc174ece005071faa6234c71bc4a75eff6d7cca8
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-05-21 20:45:50 +0200

    define endpoints #13

--- server/mvtool/endpoints.py
+++ server/mvtool/endpoints.py
@@ -13,10 +13,54 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU AGPL V3 for more details.
 
+from requests import delete
+from tornado.web import HTTPError
 from .utils.endpoint import SQLAlchemyEndpoint
 from .utils.openapi import EndpointOpenAPIMixin
 from . import schemas
 
 
+class DocumentsEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.DocumentSchema
+
+
+class JiraInstancesEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.JiraInstanceSchema
+    HIDE_OPERATIONS = ['create', 'update', 'delete']
+
+
+class JiraProjectsEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.JiraProjectSchema
+    HIDE_OPERATIONS = ['create', 'update', 'delete']
+
+    async def create(self, object_):
+        raise HTTPError(404, 'operation not implemented')
+
+    async def update(self, updated_object, id_):
+        raise HTTPError(404, 'operation not implemented')
+
+    async def delete(self, id_):
+        raise HTTPError(404, 'operation not implemented')
+
+
+class JiraIssuesEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.JiraIssueSchema
+
+    async def delete(self, id_):
+        raise HTTPError(404, 'operation not implemented')
+
+
+class TasksEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.TaskSchema
+
+
+class MeasuresEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.MeasureSchema
+
+
 class RequirementsEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
     OBJECT_SCHEMA = schemas.RequirementSchema
+
+
+class ProjectsEndpoint(SQLAlchemyEndpoint, EndpointOpenAPIMixin):
+    OBJECT_SCHEMA = schemas.ProjectSchema
