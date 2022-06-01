8e8cec5de912c0d56164a5da5dafc9c1633ff115
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-01 19:42:45 +0200

    add test case

--- server/tests/openapi.py
+++ server/tests/openapi.py
@@ -15,13 +15,24 @@
 
 import json
 from tornado.testing import AsyncHTTPTestCase
-from mvtool import MVTool
+from mvtool import MVTool, schemas, models
 
 class BaseTestCase(AsyncHTTPTestCase):
     def setUp(self):
+        # do common set up
         self.mvtool = MVTool('tests/config.yml')
         self.mvtool._prepare()
         super().setUp()
+
+        # authenticate and retrieve cookie
+        jira_credentials = self.mvtool.config.testing.jira_credentials
+        body_data = dict(
+            username=jira_credentials.username,
+            password=jira_credentials.password,
+            jira_instance=dict(url=jira_credentials.jira_instance_url))
+        response = self.fetch(
+            '/jira-user/', method='PUT', body=json.dumps(body_data))
+        self.cookie = response.headers['Set-Cookie']
         
     def tearDown(self):
         super().tearDown()
@@ -31,31 +42,50 @@ class BaseTestCase(AsyncHTTPTestCase):
         return self.mvtool.tornado_app
 
 
-class TestJiraUser(BaseTestCase):
-    def test_sign_in_success(self):
-        body_data = dict(
-            username=self.mvtool.config.testing.jira_credentials.username,
-            password=self.mvtool.config.testing.jira_credentials.password,
-            jira_instance=dict(url=self.mvtool.config.testing.jira_credentials.jira_instance_url))
-        response = self.fetch(
-            '/jira-user/', method='PUT', body=json.dumps(body_data))
+class TestJiraInstances(BaseTestCase):
+    def test_list(self):
+        response = self.fetch('/jira-instances/', method='GET')
         self.assertEqual(response.code, 200)
 
-    def test_sign_in_fail(self):
-        body_data = dict(
-            username='user',
-            password='password',
-            jira_instance=dict(url=self.mvtool.config.testing.jira_credentials.jira_instance_url))
+
+class TestJiraProjects(BaseTestCase):
+    def test_list(self):
         response = self.fetch(
-            '/jira-user/', method='PUT', body=json.dumps(body_data))
-        self.assertEqual(response.code, 401)
-
-    # def test_list_jira_projects(self):
-    #     cookie = {'Cookie': response.headers['Set-Cookie']}
-    #     response = self.fetch('/jira-user/jira-projects/', method='GET', headers=cookie)
-    #     print(response.body)
-    #     self.assertEqual(response.code, 200)
-
-    def test_sign_out(self):
-        response = self.fetch('/jira-user/', method='DELETE')
+            '/jira-user/jira-projects/', method='GET', 
+            headers={'Cookie': self.cookie})
         self.assertEqual(response.code, 200)
+
+class TestProjects(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+
+        # get JIRA project as reference for tests
+        response = self.fetch(
+            '/jira-user/jira-projects/', method='GET',
+            headers={'Cookie': self.cookie})
+        body_data = json.loads(response.body)
+        jira_projects = schemas.JiraProjectSchema(many=True).load(
+            body_data['objects'])
+        assert 0 < len(jira_projects),\
+            'For testing, at least one JIRA project must exist.'
+        self.jira_project = jira_projects[0]
+
+    def test_list(self):
+        response = self.fetch(
+            '/jira-user/projects/', method='GET',
+            headers={'Cookie': self.cookie})
+        self.assertEqual(response.code, 200)
+
+    def test_create(self):
+        project = models.Project(jira_project_id=self.jira_project.id)
+        response = self.fetch(
+            '/jira-user/projects/', method='POST',
+            headers={'Cookie': self.cookie},
+            body=schemas.ProjectSchema().dumps(project))
+        self.assertEqual(response.code, 201)
+
+    def test_update(self):
+        pass
+
+    def test_delete(self):
+        pass
