258957d39369b42caaa30ae1c5bd34b6d57abe30
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-18 16:09:49 +0200

    add file for testing project view

---
+++ tests/test_projects.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# 
+# Copyright 2022 Helmar Hutschenreuter
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation. You may obtain
+# a copy of the GNU AGPL V3 at https://www.gnu.org/licenses/.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU AGPL V3 for more details.
+
