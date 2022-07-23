ffebec39347aee57c038b66e16d77abe6a0330c4
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-23 18:54:18 +0200

    manually convert requirement to output requirement

--- mvtool/views/requirements.py
+++ mvtool/views/requirements.py
@@ -13,6 +13,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU AGPL V3 for more details.
 
+from typing import Iterator
 from fastapi import APIRouter, Depends, Response
 from fastapi_utils.cbv import cbv
 
@@ -36,33 +37,67 @@ class RequirementsView:
     @router.get(
         '/projects/{project_id}/requirements', 
         response_model=list[RequirementOutput], **kwargs)
+    def _list_requirements(
+            self, project_id: int) -> Iterator[RequirementOutput]:
+        project_output = self._projects._get_project(project_id)
+        for requirement in self.list_requirements(project_id):
+            requirement_output = RequirementOutput.from_orm(requirement)
+            requirement_output.project = project_output
+            yield requirement_output
+
     def list_requirements(self, project_id: int) -> list[Requirement]:
         return self._crud.read_all_from_db(Requirement, project_id=project_id)
 
     @router.post(
         '/projects/{project_id}/requirements', status_code=201, 
         response_model=RequirementOutput, **kwargs)
+    def _create_requirement(
+            self, project_id: int, 
+            requirement_input: RequirementInput) -> RequirementOutput:
+        requirement_output = RequirementOutput.from_orm(
+            self.create_requirement(project_id, requirement_input))
+        requirement_output.project = self._projects._get_project(project_id)
+        return requirement_output
+
     def create_requirement(
             self, project_id: int, 
-            requirement: RequirementInput) -> Requirement:
-        requirement = Requirement.from_orm(requirement)
+            requirement_input: RequirementInput) -> Requirement:
+        requirement = Requirement.from_orm(requirement_input)
         requirement.project = self._projects.get_project(project_id)
         return self._crud.create_in_db(requirement)
 
     @router.get(
-        '/requirements/{requirement_id}', response_model=RequirementOutput, **kwargs)
+        '/requirements/{requirement_id}', 
+        response_model=RequirementOutput, **kwargs)
+    def _get_requirement(self, requirement_id: int) -> RequirementOutput:
+        requirement = self.get_requirement(requirement_id)
+        requirement_output = RequirementOutput.from_orm(requirement)
+        requirement_output.project = self._projects._get_project(
+            requirement.project_id)
+        return requirement_output
+
     def get_requirement(self, requirement_id: int) -> Requirement:
         return self._crud.read_from_db(Requirement, requirement_id)
 
     @router.put(
-        '/requirements/{requirement_id}', response_model=RequirementOutput, **kwargs)
+        '/requirements/{requirement_id}', 
+        response_model=RequirementOutput, **kwargs)
+    def _update_requirement(
+            self, requirement_id: int,
+            requirement_input: RequirementInput) -> RequirementOutput:
+        requirement = self.update_requirement(requirement_id, requirement_input)
+        requirement_output = RequirementOutput.from_orm(requirement)
+        requirement_output.project = self._projects._get_project(
+            requirement.project_id)
+        return requirement_output
+
     def update_requirement(
             self, requirement_id: int, 
-            requirement_update: RequirementInput) -> Requirement:
+            requirement_input: RequirementInput) -> Requirement:
         requirement = self._crud.read_from_db(Requirement, requirement_id)
-        requirement_update = Requirement.from_orm(
-            requirement_update, update=dict(project_id=requirement.project_id))
-        return self._crud.update_in_db(requirement_id, requirement_update)
+        updated_requirement = Requirement.from_orm(
+            requirement_input, update=dict(project_id=requirement.project_id))
+        return self._crud.update_in_db(requirement_id, updated_requirement)
 
     @router.delete(
         '/requirements/{requirement_id}', status_code=204, 
