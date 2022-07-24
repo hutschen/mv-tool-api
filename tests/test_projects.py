99183041bf61ec4d72793fa2baffe511966bc062
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-24 16:29:41 +0200

    rework tests for projects view to use database

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -14,67 +14,77 @@
 # GNU AGPL V3 for more details.
 
 from unittest.mock import Mock
+from fastapi import HTTPException
 import pytest
 from mvtool.database import CRUDOperations
-from mvtool.models import ProjectInput, Project, ProjectOutput
+from mvtool.models import Project, ProjectInput, ProjectOutput
 from mvtool.views.jira_ import JiraProjectsView
 from mvtool.views.projects import ProjectsView
 
+@pytest.fixture
+def create_project(crud: CRUDOperations, project_input: ProjectInput):
+    jira_projects: JiraProjectsView = Mock()
+    jira_projects.check_jira_project_id.return_value = None
 
-def test_list_projects(jira_projects_view: JiraProjectsView, crud: CRUDOperations, project):
-    crud.read_all_from_db.return_value = [project]
-    jira_projects_view.list_jira_projects.return_value = []
+    projects = ProjectsView(jira_projects, crud)
+    return projects.create_project(project_input)
 
-    sut = ProjectsView(jira_projects_view, crud)
+def test_list_project_outputs(crud: CRUDOperations, create_project: Project):
+    jira_projects: JiraProjectsView = Mock()
+    jira_projects.list_jira_projects.return_value = []
+
+    sut = ProjectsView(jira_projects, crud)
     results = list(sut._list_projects())
 
+    assert len(results) == 1
     assert isinstance(results[0], ProjectOutput)
-    assert results[0].id == project.id
-    crud.read_all_from_db.assert_called_once()
-    jira_projects_view.list_jira_projects.assert_called_once()
+    assert results[0].id == create_project.id
+    jira_projects.list_jira_projects.assert_called_once()
 
-def test_create_project(jira_projects_view: JiraProjectsView, crud: CRUDOperations, project_input, project):
-    crud.create_in_db.return_value = project
-    jira_projects_view.check_jira_project_id.return_value = None
+def test_create_project_output(crud: CRUDOperations, project_input: ProjectInput):
+    jira_projects: JiraProjectsView = Mock()
+    jira_projects.check_jira_project_id.return_value = None
+    jira_projects.try_to_get_jira_project.return_value = None
+    project_input.jira_project_id = '1000'
 
-    sut = ProjectsView(jira_projects_view, crud)
+    sut = ProjectsView(jira_projects, crud)
     result = sut._create_project(project_input)
 
     assert isinstance(result, ProjectOutput)
-    assert result.id == project.id
-    crud.create_in_db.assert_called_once()
-    jira_projects_view.check_jira_project_id.assert_called_once()
+    jira_projects.check_jira_project_id.assert_called_with(
+        project_input.jira_project_id)
 
-def test_get_project(jira_projects_view: JiraProjectsView, crud: CRUDOperations, project):
-    crud.read_from_db.return_value = project
-    jira_projects_view.try_to_get_jira_project.return_value = None
+def test_get_project_output(crud: CRUDOperations, create_project: Project):
+    jira_projects = Mock()
+    jira_projects.try_to_get_jira_project.return_value = None
 
-    sut = ProjectsView(jira_projects_view, crud)
-    result = sut._get_project(1)
-
-    assert isinstance(result, ProjectOutput)
-    assert result.id == project.id
-    assert result.jira_project is None
-    crud.read_from_db.assert_called_once()
-    jira_projects_view.try_to_get_jira_project.assert_called_once()
-
-def test_update_project(jira_projects_view: JiraProjectsView, crud: CRUDOperations, project_input, project):
-    crud.update_in_db.return_value = project
-    jira_projects_view.check_jira_project_id.return_value = None
-
-    sut = ProjectsView(jira_projects_view, crud)
-    result = sut._update_project(project.id, project_input)
+    sut = ProjectsView(jira_projects, crud)
+    result = sut._get_project(create_project.id)
 
     assert isinstance(result, ProjectOutput)
-    assert result.id == project.id
-    crud.update_in_db.assert_called_once()
-    jira_projects_view.check_jira_project_id.assert_called_once()
 
-def test_delete_project(jira_projects_view: JiraProjectsView, crud: CRUDOperations, project):
-    crud.delete_from_db.return_value = None
+def test_update_project_output(
+        crud: CRUDOperations, project_input: ProjectInput, 
+        create_project: Project):
+    jira_projects = Mock()
+    jira_projects.check_jira_project_id.return_value = None
+    jira_projects.try_to_get_jira_project.return_value = None
 
-    sut = ProjectsView(jira_projects_view, crud)
-    result = sut.delete_project(1)
+    orig_project_name = project_input.name
+    project_input.name += ' (updated)'
+    assert project_input.name != orig_project_name
 
+    sut = ProjectsView(jira_projects, crud)
+    result = sut._update_project(create_project.id, project_input)
+
+    assert isinstance(result, ProjectOutput)
+    assert result.name == project_input.name
+
+def test_delete_project(crud: CRUDOperations, create_project: Project):
+    sut = ProjectsView(None, crud)
+    result = sut.delete_project(create_project.id)
     assert result is None
-    crud.delete_from_db.assert_called_once()
+
+    with pytest.raises(HTTPException) as exception_info:
+        sut.delete_project(create_project.id)
+        assert exception_info.value.status_code == 404
