ddc1f522baeba6f5ae7cacfedd213ad7b6901e84
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-21 12:26:21 +0200

    overwrite lookup_path to simplify URL rewrite

---
+++ mvtool/angular.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+#
+# Copyright (C) 2022 Helmar Hutschenreuter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from fastapi.staticfiles import StaticFiles
+
+
+class AngularFiles(StaticFiles):
+    def lookup_path(self, path: str) -> tuple[str, os.stat_result | None]:
+        full_path, stat_result = super().lookup_path(path)
+        if stat_result is None:
+            return super().lookup_path("index.html")
+        else:
+            return full_path, stat_result
