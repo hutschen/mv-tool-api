d29e081f8f539a0e014cebd640e26c81cd2c7796
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-30 21:35:32 +0200

    enable other connections than sqlite connections

--- mvtool/database.py
+++ mvtool/database.py
@@ -33,12 +33,18 @@ class __State:
 
 def setup_engine(config: Config):
     if __State.engine is None:
-        __State.engine = create_engine(
-            config.database.url,
-            connect_args={"check_same_thread": False},  # Needed for SQLite
-            echo=config.database.echo,
-            poolclass=StaticPool,
-        )
+        if config.database.url.startswith("sqlite"):
+            __State.engine = create_engine(
+                config.database.url,
+                connect_args={"check_same_thread": False},  # Needed for SQLite
+                echo=config.database.echo,
+                poolclass=StaticPool,  # Maintain a single connection for all threads
+            )
+        else:
+            __State.engine = create_engine(
+                config.database.url,
+                echo=config.database.echo,
+            )
     return __State.engine
 
 
