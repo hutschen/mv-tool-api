5ac366f7c95e783de3dababbb3cb63370db5850c
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-09-27 18:24:32 +0200

    configure alembic for the use with sqlmodel
    see https://stackoverflow.com/a/69063829

--- migration/env.py
+++ migration/env.py
@@ -18,7 +18,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from mvtool.models import SQLModel
+
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +66,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
