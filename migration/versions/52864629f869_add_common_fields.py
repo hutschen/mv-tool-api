5fff6899936e7daeac9379d245f3cbf02c3eb8dd
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-05 19:21:32 +0200

    adjust upgrade to fully match dll when upgrading from an empty database

--- migration/versions/52864629f869_add_common_fields.py
+++ migration/versions/52864629f869_add_common_fields.py
@@ -8,8 +8,6 @@ Create Date: 2022-10-04 20:06:54.945859
 from datetime import datetime
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy import table, column
-from sqlalchemy.exc import OperationalError
 
 
 # revision identifiers, used by Alembic.
@@ -22,23 +20,42 @@ depends_on = None
 def upgrade() -> None:
     table_names = ["requirement", "project", "measure", "gs_baustein", "document"]
     for table_name in table_names:
-        # add columns: created, updated
-        op.add_column(table_name, sa.Column("created", sa.DateTime(), nullable=True))
-        op.add_column(table_name, sa.Column("updated", sa.DateTime(), nullable=True))
-
-        # set default values for created, updated
-        table_ = table(table_name, column("created"), column("updated"))
-        op.execute(
-            table_.update().values(created=datetime.now(), updated=datetime.now())
+        # check if table is empty
+        row_count = (
+            op.get_bind()
+            .execute(sa.select([sa.func.count()]).select_from(sa.table(table_name)))
+            .scalar()
         )
+        if row_count == 0:
+            # table is empty, add columns with nullable=False
+            op.add_column(
+                table_name, sa.Column("created", sa.DateTime(), nullable=False)
+            )
+            op.add_column(
+                table_name, sa.Column("updated", sa.DateTime(), nullable=False)
+            )
 
-        # set not null constraint for columns: created, updated
-        try:
-            op.alter_column(table_name, "created", nullable=False)
-            op.alter_column(table_name, "updated", nullable=False)
-        except OperationalError:
-            # sqlite does not support altering columns
-            pass
+        else:
+            # table is not empty, add columns with nullable=True
+            op.add_column(
+                table_name, sa.Column("created", sa.DateTime(), nullable=True)
+            )
+            op.add_column(
+                table_name, sa.Column("updated", sa.DateTime(), nullable=True)
+            )
+
+            # set default values for columns: created, updated
+            op.execute(
+                sa.table(table_name, sa.column("created"), sa.column("updated"))
+                .update()
+                .values(created=datetime.now(), updated=datetime.now())
+            )
+
+            # set not null constraint for columns: created, updated
+            if op.get_bind().engine.name != "sqlite":
+                # sqlite does not support altering columns
+                op.alter_column(table_name, "created", nullable=False)
+                op.alter_column(table_name, "updated", nullable=False)
 
 
 def downgrade() -> None:
