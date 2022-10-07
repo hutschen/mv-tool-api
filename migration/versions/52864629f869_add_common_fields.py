77e302782cf32c088611518443d2e4afed75201a
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-07 21:01:08 +0200

    refactor migration to add common fields

--- migration/versions/52864629f869_add_common_fields.py
+++ migration/versions/52864629f869_add_common_fields.py
@@ -18,44 +18,23 @@ depends_on = None
 
 
 def upgrade() -> None:
-    table_names = ["requirement", "project", "measure", "gs_baustein", "document"]
-    for table_name in table_names:
-        # check if table is empty
-        row_count = (
-            op.get_bind()
-            .execute(sa.select([sa.func.count()]).select_from(sa.table(table_name)))
-            .scalar()
+    for table_name in ["requirement", "project", "measure", "gs_baustein", "document"]:
+        # add columns with nullable=True
+        op.add_column(table_name, sa.Column("created", sa.DateTime(), nullable=True))
+        op.add_column(table_name, sa.Column("updated", sa.DateTime(), nullable=True))
+
+        # set values for columns: created, updated
+        now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        op.execute(
+            f"""
+            UPDATE {table_name} SET created = '{now_str}', updated = '{now_str}'
+            """
         )
-        if row_count == 0:
-            # table is empty, add columns with nullable=False
-            op.add_column(
-                table_name, sa.Column("created", sa.DateTime(), nullable=False)
-            )
-            op.add_column(
-                table_name, sa.Column("updated", sa.DateTime(), nullable=False)
-            )
 
-        else:
-            # table is not empty, add columns with nullable=True
-            op.add_column(
-                table_name, sa.Column("created", sa.DateTime(), nullable=True)
-            )
-            op.add_column(
-                table_name, sa.Column("updated", sa.DateTime(), nullable=True)
-            )
-
-            # set default values for columns: created, updated
-            op.execute(
-                sa.table(table_name, sa.column("created"), sa.column("updated"))
-                .update()
-                .values(created=datetime.now(), updated=datetime.now())
-            )
-
-            # set not null constraint for columns: created, updated
-            if op.get_bind().engine.name != "sqlite":
-                # sqlite does not support altering columns
-                op.alter_column(table_name, "created", nullable=False)
-                op.alter_column(table_name, "updated", nullable=False)
+        # make columns not nullable
+        with op.batch_alter_table(table_name, schema=None) as batch_op:
+            batch_op.alter_column("created", nullable=False)
+            batch_op.alter_column("updated", nullable=False)
 
 
 def downgrade() -> None:
