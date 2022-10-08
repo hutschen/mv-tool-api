87e048a7e48a8477ae3cbdf2dc5fb344dd20e53c
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-08 14:09:58 +0200

    set naming convention for constraints
    see https://alembic.sqlalchemy.org/en/latest/naming.html#the-importance-of-naming-constraints

--- mvtool/database.py
+++ mvtool/database.py
@@ -26,6 +26,17 @@ from .config import DatabaseConfig
 SelectOfScalar.inherit_cache = True
 Select.inherit_cache = True
 
+# configure naming conventions to make migration easier
+# see https://alembic.sqlalchemy.org/en/latest/naming.html#the-importance-of-naming-constraints
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+SQLModel.metadata.naming_convention = naming_convention
+
 
 class __State:
     engine = None
