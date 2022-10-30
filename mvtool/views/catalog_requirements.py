10824b8d99695c4db34366010c70fd2b2aaa4875
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-30 11:13:25 +0100

    implement crud operations of catalog requirements view

--- mvtool/views/catalog_requirements.py
+++ mvtool/views/catalog_requirements.py
@@ -18,8 +18,9 @@
 from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 
+from ..errors import NotFoundError
 from ..database import CRUDOperations
-from ..models import CatalogRequirement
+from ..models import CatalogRequirement, CatalogRequirementInput
 from .catalog_modules import CatalogModulesView
 
 router = APIRouter()
@@ -38,21 +39,42 @@ class CatalogRequirementsView:
         self._crud = crud
         self._session = self._crud.session
 
-    def list_catalog_requirements(self, catalog_module_id: int):
-        pass
+    def list_catalog_requirements(
+        self, catalog_module_id: int
+    ) -> list[CatalogRequirement]:
+        return self._crud.read_all_from_db(
+            CatalogRequirement, catalog_module_id=catalog_module_id
+        )
 
     def create_catalog_requirement(
-        self, catalog_module_id: int, catalog_requirement: CatalogRequirement
-    ):
-        pass
+        self, catalog_module_id: int, catalog_requirement_input: CatalogRequirementInput
+    ) -> CatalogRequirement:
+        catalog_requirement = CatalogRequirement.from_orm(catalog_requirement_input)
+        catalog_requirement.catalog_module = self._catalog_modules.get_catalog_module(
+            catalog_module_id
+        )
+        return self._crud.create_in_db(catalog_requirement)
 
-    def get_catalog_requirement(self, catalog_requirement_id: int):
-        pass
+    def get_catalog_requirement(
+        self, catalog_requirement_id: int
+    ) -> CatalogRequirement:
+        return self._crud.read_from_db(CatalogRequirement, catalog_requirement_id)
 
     def update_catalog_requirement(
-        self, catalog_requirement_id: int, catalog_requirement: CatalogRequirement
-    ):
-        pass
+        self,
+        catalog_requirement_id: int,
+        catalog_requirement_input: CatalogRequirementInput,
+    ) -> CatalogRequirement:
+        catalog_requirement = self._session.get(
+            CatalogRequirement, catalog_requirement_id
+        )
+        if not catalog_requirement:
+            cls_name = CatalogRequirement.__name__
+            raise NotFoundError(f"No {cls_name} with id={catalog_requirement_id}.")
+        for key, value in catalog_requirement_input.dict().items():
+            setattr(catalog_requirement, key, value)
+        self._session.flush()
+        return catalog_requirement
 
-    def delete_catalog_requirement(self, catalog_requirement_id: int):
-        pass
+    def delete_catalog_requirement(self, catalog_requirement_id: int) -> None:
+        return self._crud.delete_from_db(CatalogRequirement, catalog_requirement_id)
