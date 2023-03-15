898590a51fb13723384a8f7f7f7448bc38eb69c2
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-03-15 13:21:45 +0100

    use generators to iterate over children of columns definition

--- mvtool/tables/common.py
+++ mvtool/tables/common.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Any, Generic, Iterable, Iterator, NamedTuple, TypeVar
+from typing import Any, Generator, Generic, Iterable, Iterator, NamedTuple, TypeVar
 
 import pandas as pd
 from pydantic import BaseModel
@@ -80,19 +80,19 @@ class ColumnsDef(Generic[I, E]):
 
     @property
     def is_export(self) -> bool:
-        return bool(self.children_for_export)
+        return any(c.is_export for c in self.children)
 
     @property
     def is_import(self) -> bool:
-        return bool(self.children_for_import)
+        return any(c.is_import for c in self.children)
 
     @property
-    def children_for_export(self) -> list[ColumnDef]:
-        return [c for c in self.children if c.is_export]
+    def children_for_export(self) -> "Generator[ColumnDef | ColumnsDef]":
+        return (c for c in self.children if c.is_export)
 
     @property
-    def children_for_import(self) -> list[ColumnDef]:
-        return [c for c in self.children if c.is_import]
+    def children_for_import(self) -> "Generator[ColumnDef | ColumnsDef]":
+        return (c for c in self.children if c.is_import)
 
     def export_to_row(self, obj: E) -> Iterator[Cell]:
         for child in self.children_for_export:
@@ -105,6 +105,7 @@ class ColumnsDef(Generic[I, E]):
                     yield Cell(f"{self.label} {child.label}", value)
 
     def export_to_dataframe(self, objs: Iterable[E]) -> pd.DataFrame:
+        # FIXME: column order is not deterministic, as not all columns are present in all rows.
         return pd.DataFrame(dict(self.export_to_row(o)) for o in objs)
 
     def import_from_row(self, row: Iterable[Cell]) -> I:
@@ -141,7 +142,7 @@ class ColumnsDef(Generic[I, E]):
             for columns_def in columns_defs:
                 model_kwargs[columns_def.attr_name] = columns_def.import_from_row(row)
 
-            return self.import_model(**model_kwargs)
+            return self.import_model(**model_kwargs)  # TODO: handle validation errors
 
     def import_from_dataframe(self, df: pd.DataFrame) -> Iterator[I]:
         lables = df.columns.to_list()
