2aeb68ef7767c579c53ba2f534da6c555a297376
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-03-17 21:09:28 +0100

    move measure related models into measure module of models package

--- mvtool/models/__init__.py
+++ mvtool/models/__init__.py
@@ -29,72 +29,13 @@ from .jira_ import (
     JiraIssueInput,
     JiraIssue,
 )
-
-
-class AbstractMeasureInput(AbstractComplianceInput):
-    reference: str | None
-    summary: str
-    description: str | None
-    completion_status: constr(regex=r"^(open|in progress|completed)$") | None
-    completion_comment: str | None
-    verification_method: constr(regex=r"^(I|T|R)$") | None
-    verification_status: constr(
-        regex=r"^(verified|partially verified|not verified)$"
-    ) | None
-    verification_comment: str | None
-
-    @validator("completion_comment")
-    def completion_comment_validator(cls, v, values):
-        return cls._dependent_field_validator(
-            "completion_comment", "completion_status", v, values
-        )
-
-    @validator("verification_status")
-    def verification_status_validator(cls, v, values):
-        return cls._dependent_field_validator(
-            "verification_status", "verification_method", v, values
-        )
-
-    @validator("verification_comment")
-    def verification_comment_validator(cls, v, values):
-        return cls._dependent_field_validator(
-            "verification_comment", "verification_method", v, values
-        )
-
-
-class MeasureInput(AbstractMeasureInput):
-    document_id: int | None
-    jira_issue_id: str | None
-
-
-class Measure(MeasureInput, CommonFieldsMixin, table=True):
-    requirement_id: int | None = Field(default=None, foreign_key="requirement.id")
-    requirement: "Requirement" = Relationship(
-        back_populates="measures", sa_relationship_kwargs=dict(lazy="joined")
-    )
-    document_id: int | None = Field(default=None, foreign_key="document.id")
-    document: "Document" = Relationship(
-        back_populates="measures", sa_relationship_kwargs=dict(lazy="joined")
-    )
-
-    _get_jira_issue: Callable = PrivateAttr()
-
-    @property
-    def jira_issue(self) -> JiraIssue | None:
-        if self.jira_issue_id is None:
-            return None
-
-        return getattr(self, "_get_jira_issue")(self.jira_issue_id)
-
-    @property
-    def completion_status_hint(self):
-        if self.compliance_status not in ("C", "PC", None):
-            return None
-
-        if self.jira_issue and self.jira_issue.status.completed:
-            return "completed"
-        else:
-            return self.completion_status
+from .measures import (
+    AbstractMeasureInput,
+    MeasureInput,
+    Measure,
+    MeasureRepresentation,
+    MeasureOutput,
+)
 
 
 class AbstractRequirementInput(SQLModel):
@@ -416,23 +357,6 @@ class RequirementOutput(AbstractRequirementInput):
     verification_progress: confloat(ge=0, le=1) | None
 
 
-class MeasureRepresentation(SQLModel):
-    id: int
-    reference: str | None
-    summary: str
-
-
-class MeasureOutput(MeasureRepresentation):
-    description: str | None
-    compliance_status: str | None
-    compliance_comment: str | None
-    completion_status: str | None
-    completion_status_hint: str | None
-    completion_comment: str | None
-    verification_status: str | None
-    verification_method: str | None
-    verification_comment: str | None
-    requirement: RequirementOutput
-    jira_issue_id: str | None
-    jira_issue: JiraIssue | None
-    document: DocumentOutput | None
+MeasureOutput.update_forward_refs(
+    RequirementOutput=RequirementOutput, DocumentOutput=DocumentOutput
+)
---
+++ mvtool/models/measures.py
@@ -0,0 +1,114 @@
+# coding: utf-8
+#
+# Copyright (C) 2023 Helmar Hutschenreuter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from typing import TYPE_CHECKING, Callable
+from pydantic import PrivateAttr, constr, validator
+from sqlmodel import Field, Relationship, SQLModel
+from mvtool.models.common import AbstractComplianceInput, CommonFieldsMixin
+from mvtool.models.jira_ import JiraIssue
+
+if TYPE_CHECKING:
+    from . import Requirement, Document, RequirementOutput, DocumentOutput
+
+
+class AbstractMeasureInput(AbstractComplianceInput):
+    reference: str | None
+    summary: str
+    description: str | None
+    completion_status: constr(regex=r"^(open|in progress|completed)$") | None
+    completion_comment: str | None
+    verification_method: constr(regex=r"^(I|T|R)$") | None
+    verification_status: constr(
+        regex=r"^(verified|partially verified|not verified)$"
+    ) | None
+    verification_comment: str | None
+
+    @validator("completion_comment")
+    def completion_comment_validator(cls, v, values):
+        return cls._dependent_field_validator(
+            "completion_comment", "completion_status", v, values
+        )
+
+    @validator("verification_status")
+    def verification_status_validator(cls, v, values):
+        return cls._dependent_field_validator(
+            "verification_status", "verification_method", v, values
+        )
+
+    @validator("verification_comment")
+    def verification_comment_validator(cls, v, values):
+        return cls._dependent_field_validator(
+            "verification_comment", "verification_method", v, values
+        )
+
+
+class MeasureInput(AbstractMeasureInput):
+    document_id: int | None
+    jira_issue_id: str | None
+
+
+class Measure(MeasureInput, CommonFieldsMixin, table=True):
+    requirement_id: int | None = Field(default=None, foreign_key="requirement.id")
+    requirement: "Requirement" = Relationship(
+        back_populates="measures", sa_relationship_kwargs=dict(lazy="joined")
+    )
+    document_id: int | None = Field(default=None, foreign_key="document.id")
+    document: "Document" = Relationship(
+        back_populates="measures", sa_relationship_kwargs=dict(lazy="joined")
+    )
+
+    _get_jira_issue: Callable = PrivateAttr()
+
+    @property
+    def jira_issue(self) -> JiraIssue | None:
+        if self.jira_issue_id is None:
+            return None
+
+        return getattr(self, "_get_jira_issue")(self.jira_issue_id)
+
+    @property
+    def completion_status_hint(self):
+        if self.compliance_status not in ("C", "PC", None):
+            return None
+
+        if self.jira_issue and self.jira_issue.status.completed:
+            return "completed"
+        else:
+            return self.completion_status
+
+
+class MeasureRepresentation(SQLModel):
+    id: int
+    reference: str | None
+    summary: str
+
+
+class MeasureOutput(MeasureRepresentation):
+    description: str | None
+    compliance_status: str | None
+    compliance_comment: str | None
+    completion_status: str | None
+    completion_status_hint: str | None
+    completion_comment: str | None
+    verification_status: str | None
+    verification_method: str | None
+    verification_comment: str | None
+    requirement: "RequirementOutput"
+    jira_issue_id: str | None
+    jira_issue: JiraIssue | None
+    document: "DocumentOutput | None"
