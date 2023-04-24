3c8c5c09d21a3f4c8ffbe232537305f98fda1d61
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-24 20:50:17 +0200

    add function to convert uploaded excel file in a data frame

--- mvtool/tables/handlers.py
+++ mvtool/tables/handlers.py
@@ -17,9 +17,11 @@
 
 from typing import Callable
 
-from fastapi import Depends, Query
+import pandas as pd
+from fastapi import Depends, HTTPException, Query
 
 from ..auth import get_jira
+from ..utils.temp_file import copy_upload_to_temp_file
 from .common import ColumnGroup
 
 
@@ -42,3 +44,30 @@ def get_export_labels_handler(get_columns: Callable) -> Callable:
         return columns.export_labels
 
     return handler
+
+
+def get_uploaded_dataframe(
+    temp_file=Depends(copy_upload_to_temp_file),
+    skip_duplicates: bool = False,
+) -> pd.DataFrame:
+    # Default list of na_values of pandas without 'N/A'
+    na_values = [
+        # fmt: off
+        "", "#N/A", "#N/A N/A", "#NA", "-1.#IND", "-1.#QNAN", "-NaN", "-nan",
+        "1.#IND", "1.#QNAN", "<NA>", "NA", "NULL", "NaN", "None", "n/a", "nan", "null"
+        # fmt: on
+    ]
+
+    try:
+        df = pd.read_excel(
+            temp_file, engine="openpyxl", keep_default_na=False, na_values=na_values
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid file. Only Excel files are supported: " + str(e),
+        )
+
+    if skip_duplicates:
+        df.drop_duplicates(keep="last", inplace=True)
+    return df
